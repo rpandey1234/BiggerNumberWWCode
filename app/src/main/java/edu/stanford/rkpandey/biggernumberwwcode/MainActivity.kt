0a5e333078f39b1d47b9b4fb2e7f0547e4b50327
Rahul Pandey <hello@rkpandey.com>
2020-05-11 01:15:19 -0700

    Initial commit, just the layout file

---
+++ app/src/main/java/edu/stanford/rkpandey/biggernumberwwcode/MainActivity.kt
@@ -0,0 +1,12 @@
+package edu.stanford.rkpandey.biggernumberwwcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
